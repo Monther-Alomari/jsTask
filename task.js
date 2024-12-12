b910d93a100e13cf154416a3d3980b1e26f08a81
Monther Alomari <montheralomari896@gmail.com>
2024-12-12 11:51:19 +0300

    add the solution

---
+++ task.js
@@ -0,0 +1,14 @@
+let username = prompt("Enter your username:");
+let password = prompt("Enter your password:");
+let attempts = 1;
+
+while (password !== "123" && attempts < 5) {
+  password = prompt("Incorrect password. Please enter it again:");
+  attempts++;
+}
+
+if (password === "123") {
+  alert("Welcome, ${username}");
+} else {
+  alert("Please try again later.");
+}
